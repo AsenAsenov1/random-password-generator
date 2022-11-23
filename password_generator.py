13e622778208433ad2fa0e61b9cfa5ca0a660317
Asen Asenov <asen991@gmail.com>
2022-11-23 15:33:08 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -4,7 +4,9 @@ import random
 def generate_pass():
     special_characters = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=', '`', '|', '\\', '(', ')',
                           '{', '}', '[', ']', ':', ';', '"', "'", '<', '>', ',', '.', '?', '/']
+    # ASCII chars [a-z]
     lower_letters = [chr(x) for x in range(97, 123)]
+    # ASCII chars [A-Z]
     upper_letters = [chr(x) for x in range(65, 91)]
     digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     all_symbols = [special_characters, lower_letters, upper_letters, digits]
